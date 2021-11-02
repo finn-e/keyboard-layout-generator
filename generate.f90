34aaefc3f8b8735c7457a0087a3f06889137b5cb
Fin o'Flaherty <fin@oflaherty.is>
2021-11-02 17:02:22 -0400

    Test program for a test commit.

---
+++ generate.f90
@@ -0,0 +1,15 @@
+program generate
+
+! This simple program adds two addNumbers
+    implicit none
+
+! Type declarations
+    real :: a, b, result
+
+! Executable statements
+    a = 12.0
+    b = 15.0
+    result = a + b
+    print *, 'The total is', result
+
+end program generate
